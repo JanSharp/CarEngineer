4b506ff44b1b8aac570e1d5dc8d047aa4580e856
JanSharp <steinmeister36@gmail.com>
2021-06-20 05:59:05 +0200

    Add setting to enable/disable the shortcut

--- data.lua
+++ data.lua
@@ -7,38 +7,43 @@ data:extend{
     key_sequence = "ALT + G",
     action = "lua",
   },
-  {
-    type = "shortcut",
-    name = "CarEngineer-suicide",
-    localised_name = {"CarEngineer.who-knew"},
-    associated_control_input = "CarEngineer-suicide",
-    action = "lua",
-    icon =
-    {
-      filename = "__CarEngineer__/graphics/suicide-x32.png",
-      priority = "extra-high-no-scale",
-      size = 32,
-      scale = 0.5,
-      mipmap_count = 2,
-      flags = {"gui-icon"},
-    },
-    small_icon =
-    {
-      filename = "__CarEngineer__/graphics/suicide-x24.png",
-      priority = "extra-high-no-scale",
-      size = 24,
-      scale = 0.5,
-      mipmap_count = 2,
-      flags = {"gui-icon"},
-    },
-    disabled_small_icon =
-    {
-      filename = "__CarEngineer__/graphics/suicide-x24-white.png",
-      priority = "extra-high-no-scale",
-      size = 24,
-      scale = 0.5,
-      mipmap_count = 2,
-      flags = {"gui-icon"},
-    },
-  },
 }
+
+if settings.startup["CarEngineer-enable-shortcut"].value then
+  data:extend{
+    {
+      type = "shortcut",
+      name = "CarEngineer-suicide",
+      localised_name = {"CarEngineer.who-knew"},
+      associated_control_input = "CarEngineer-suicide",
+      action = "lua",
+      icon =
+      {
+        filename = "__CarEngineer__/graphics/suicide-x32.png",
+        priority = "extra-high-no-scale",
+        size = 32,
+        scale = 0.5,
+        mipmap_count = 2,
+        flags = {"gui-icon"},
+      },
+      small_icon =
+      {
+        filename = "__CarEngineer__/graphics/suicide-x24.png",
+        priority = "extra-high-no-scale",
+        size = 24,
+        scale = 0.5,
+        mipmap_count = 2,
+        flags = {"gui-icon"},
+      },
+      disabled_small_icon =
+      {
+        filename = "__CarEngineer__/graphics/suicide-x24-white.png",
+        priority = "extra-high-no-scale",
+        size = 24,
+        scale = 0.5,
+        mipmap_count = 2,
+        flags = {"gui-icon"},
+      },
+    },
+  }
+end
---
+++ settings.lua
@@ -0,0 +1,9 @@
+
+data:extend{
+  {
+    type = "bool-setting",
+    name = "CarEngineer-enable-shortcut",
+    setting_type = "startup",
+    default_value = false,
+  }
+}
